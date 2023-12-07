e491ee8654a74e931b525345d66f44393b03b9f4
Paulina Kattilavaara <paulinakatt@hotmail.com>
2023-12-07 13:58:18 +0100

    added notes section that is editable by the user

--- script/script.js
+++ script/script.js
@@ -1,3 +1,4 @@
+/* ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ */
 /*NUMBER 2: MAKE THE HEADING EDITABLE
  Getting a connection with the h1. */
 const editable = document.getElementById('editable');
@@ -13,3 +14,17 @@ window.onload = () => {
     let editedText = localStorage.getItem("text");
     editable.innerHTML = editedText;
 };
+/* ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ */
+/* NUMBER 6: CREATE TEXTAREA FOR USERS NOTES
+Getting a connection with textarea */
+const notesInput = document.getElementById("notesInput");
+
+// Save inputvalue in localStorage.
+notesInput.oninput = () => {localStorage.setItem("notes", notesInput.value)};
+
+// Load saved text when page loads.
+window.onload = () => {
+    let editedNotes = localStorage.getItem("notes");
+    notesInput.textContent = editedNotes;
+}
+/* ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ */
