42a62fed950b9446d121b4626d2ccf43bd791518
Paulina Kattilavaara <paulinakatt@hotmail.com>
2023-12-13 15:09:27 +0100

    added new section with random activity generator from api

---
+++ script/random.js
@@ -0,0 +1,26 @@
+/* ~ ~ ~ ~ NUMBER 5 : ADD EXTERNAL API DATA ~ ~ ~ ~ */
+
+// Fetch  API
+const url = "http://www.boredapi.com/api/activity/";
+async function getApi(url) {
+  const response = await fetch(url);
+  if (response.ok) {
+    let data = await response.json();
+    console.log(data);
+    // Get connection with p with id random that will display activity
+    const random = document.getElementById("random");
+    random.textContent = data.activity;
+    // Get connection with the update arrow so it can be updated
+    const update = document.getElementById("update");
+    update.addEventListener("click", function() {
+      // Update page when clicked
+      location.reload();
+    })
+    return data;
+  } else {
+    console.log(response);
+  }
+}
+getApi(url);
+
+/* ~ ~ ~ ~ ~ ~ ~ ~ ~ E N D : 5 ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ */
